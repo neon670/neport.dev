1829e46903094409baa1002675da3c2400cdef0c
noe nevarez <closner_nevarez@hotmail.com>
2015-11-04 18:14:26 -0600

    spelling

--- app/views/hello.php
+++ app/views/hello.php
@@ -75,7 +75,7 @@
     <header>
         <div class="header-content">
             <div class="header-content-inner">
-                <h1>Enchancing your online presence</h1>
+                <h1>Enhancing your online presence</h1>
                 <hr>
                 <p>Let me assist you on making what you invision come true</p>
                 <a href="#portfolio" class="btn btn-primary btn-xl page-scroll">Find Out More</a>
